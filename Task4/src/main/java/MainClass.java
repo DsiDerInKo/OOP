813ebfe3f0e7a323af9581b4ed5de978b5099e0b
DsiDerInKo <nikbush8@gmail.com>
2023-03-07 00:51:02 +0300

    json on

--- Task4/src/main/java/MainClass.java
+++ Task4/src/main/java/MainClass.java
@@ -1,16 +1,14 @@
-
-
-
 import java.io.IOException;
 import java.util.Scanner;
 
+import com.google.gson.*;
 
 public class MainClass {
 
 
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
-        int xDroneStart
+
     }
 
 }

ef859145b9606d6b3a63181d73cb17e4dcb3a4b7
DsiDerInKo <nikbush8@gmail.com>
2023-03-18 21:00:34 +0300

    inital

---
+++ Task6/src/main/java/app/note/interfaces/NoteDAO.java
@@ -0,0 +1,23 @@
+package app.note.interfaces;
+
+import app.note.Note;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface NoteDAO {
+
+    Note add(Note card);
+
+    Note delete(String header);
+
+    void deleteAll();
+
+    Optional<Note> getNote(String header);
+
+    Note changeHeader(String header, String newHeader);
+
+    List<Note> getNotes();  //    ???  (get specified List of Entity
+    //                                                        grouped by some condition)
+
+}

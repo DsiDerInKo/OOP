1f6abe6b3bc64d6431796ad41b1860265efd1611
DsiDerInKo <nikbush8@gmail.com>
2023-03-21 12:43:12 +0300

    Fixes

--- Task4/src/main/java/bushuev/MainClass.java
+++ Task4/src/main/java/bushuev/MainClass.java
@@ -1,77 +1,49 @@
 package bushuev;
 
-import com.google.gson.*;
-
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+
+import com.google.gson.*;
+import com.google.gson.annotations.*;
 
 public class MainClass {
 
-    public static String parse(String jsonLine) {
-        JsonElement jelement = new JsonParser().parse(jsonLine);
-        JsonObject  jobject = jelement.getAsJsonObject();
-        jobject = jobject.getAsJsonObject("data");
-        JsonArray jarray = jobject.getAsJsonArray("translations");
-        jobject = jarray.get(0).getAsJsonObject();
-        return jobject.get("translatedText").getAsString();
-    }
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) {
+        Gson gson = new Gson();
 
-        /*
-        * JsonTry tmp = new JsonTry();
+        Student obj = gson.fromJson(new FileReader("C:\\Prog\\java_prod\\OOP\\Task4\\src\\main\\java\\bushuev\\test.json"), Student.class);
 
-        Gson newObj = tmp.getGsonObj();
+        System.out.println(obj.toString());
 
-        try {
-            Gson gson = new Gson();
-            Reader reader = Files.newBufferedReader(Paths.get("test.json"));
-
-            JsonTry.Persone man = newObj.fromJson(reader,JsonTry.Persone.class);
-            Map<?, ?> map = gson.fromJson(reader, Map.class);
-            System.out.println(man);
-
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        * */
-
-        Gson g = new Gson();
-        Student p = g.fromJson("{\n" +
-                "  \"name\": \"Victor Ivan\",\n" +
-                "  \"age\": 19,\n" +
-                "}\n", Student.class);
+        Student nikita = new Student("Nikita");
+        Gson gson2 = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson2.toJson(nikita);
 
     }
 
-    class Student {
-        private String name;
+    class Food {
+        private String type;
+        private int amount;
+    }
+
+    static class Student {
+        private final String name;
         private int age;
-        public Student(){}
 
-        public String getName() {
-            return name;
-        }
+        @SerializedName("food")
+        private List<Food> foodEaten;
 
-        public void setName(String name) {
-            this.name = name;
-        }
+        private transient int nut;
 
-        public int getAge() {
-            return age;
-        }
-
-        public void setAge(int age) {
-            this.age = age;
+        public Student(String names) {
+            name = names;
         }
 
         public String toString() {
-            return "Student [ name: "+name+", age: "+ age+ " ]";
+            return "Student [name: " + name + ", age: " + age + ", foodEaten: " + foodEaten + " ]";
         }
     }
+
 }

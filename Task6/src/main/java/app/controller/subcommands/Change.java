d593e549d7566d7ab5d2bd93bad6106b66e16b86
DsiDerInKo <nikbush8@gmail.com>
2023-04-05 10:26:13 +0300

    progress with pico changed logic, added subcommands

---
+++ Task6/src/main/java/app/controller/subcommands/Change.java
@@ -0,0 +1,4 @@
+package app.controller.subcommands;
+
+public class Change {
+}
--- Task6/src/main/java/app/note/interfaces/Controller.java
+++ Task6/src/main/java/app/note/interfaces/Controller.java
@@ -2,6 +2,8 @@ package app.note.interfaces;
 
 import app.note.Note;
 
+import java.util.concurrent.Callable;
+
 public interface Controller {
 
     //Note getNote() throws Exception;
